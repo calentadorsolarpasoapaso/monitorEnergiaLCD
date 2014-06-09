f4ecb59711495852640cdae00d81712f725925b4
josemiguel1999 <jose.miguel.jimenez.villanueva@gmail.com>
2014-06-09 07:25:03 +0200

    Ajuste
    Se ajusta el consumo.

--- monitorEnergiaLCD.ino
+++ monitorEnergiaLCD.ino
@@ -93,7 +93,7 @@ void loop()
 
 //  realPower=-2;  
   //Margen de watios para que no suene por la noche
-  realPower+=30;
+  realPower+=80;
   Serial.println(supplyVoltage);
   Serial.println(realPower);
   
